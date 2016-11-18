43585489773e37d595a65dc8670300b35871d49c
Kapil Kaushik <linkwithkk@gmail.com>
2016-11-18 18:59:50 +0530

    1.2.4.2 changes

---
+++ R/Core_Utilities.R
@@ -0,0 +1,106 @@
+# (c) 2015 Infosys Limited. All rights reserved.
+#  Except for any free or open source software  components embedded in this Infosys
+#  Proprietary software program("Program"), this Program is  protected by copyright
+#  laws, International treaties and other pending or existing intellectual property
+#  rights in India,  the United  States  and other  countries.  Except as expressly
+#  permitted, any unauthorized  reproduction  storage, photocopying,  recording  or
+#  otherwise),  or any distribution  of  this  Program,  or any portion of  it, may
+#  result  in  severe  civil  and criminal penalties, and will be prosecuted to the
+#  maximum extent possible under the law.
+
+
+getPasswordFromUser<-function()
+{
+  # get the password from user
+  sysname<-Sys.info()[["sysname"]]
+  ifRstudio<- Sys.getenv("RSTUDIO")
+  if(ifRstudio == "1")
+  {
+    password<-.rs.askForPassword("")
+    
+    
+  }
+  
+  else{
+    if(sysname=="Linux")
+    {
+      password<-get_console_pwd()
+    }
+    else{
+      password<-get_ui_pwd()
+    }
+  }
+  
+  return(password)
+  
+}
+
+uploadToHDFS<-function(inputFilePath,hdfsFilePath)
+{
+  system (paste("$HADOOP_CMD fs -put -f",inputFilePath,hdfsFilePath))
+}
+
+deleteHdfsDirectory<-function(parentDir,dirName)
+{
+  path<-paste0(parentDir,dirName)
+  system(paste0("$HADOOP_CMD dfs -rm -r ",path))
+  path
+}
+deleteLocalDirectory<-function(dirName)
+{
+  dir<-paste(getwd(),dirName,sep="")
+  #system(paste("$HADOOP_CMD dfs -rm -r ",dir,sep="")) ## delete the hdfs dir created from ngram code - by local path
+  system(paste("rm -r ",dir,sep=""))
+  dir
+}
+createHdfsDirectory<-function(parentDir,dirName)
+{
+  #TODO -error handling - if dir already exists then don't create one
+  #TODO -error handling - if parent dir doesn't exists then create one /user/
+  
+  path<-paste0(parentDir,dirName)
+  system(paste0("$HADOOP_CMD dfs -mkdir -p ",path))
+  print(paste0("HDFS directory path is ",path))
+  path
+}
+createLocalDirectory<-function(parentDir,dirName)
+{
+  #TODO -error handling - if dir already exists then don't create one
+  #TODO -error handling - if dir doesn't exists then create one
+  dir<-paste0(parentDir,dirName)
+  system(paste0("mkdir -p ",dir))
+  print(paste0("Local directory path is ",dir))
+  dir
+  
+}
+
+timeFormat<-function(timestamp, myPattern)
+{
+  timestamp<-sub(timestamp, pattern = " [[:alpha:]]*$", replacement = "")
+  timestamp<-gsub(myPattern, "_", timestamp) 
+  timestamp
+}
+
+readInputFile<-function(inputPath, readSep)
+{
+  data<-read.csv(pipe(paste("$HADOOP_CMD dfs -cat ",inputPath,sep="")),header = F,sep = readSep)
+  data
+  
+}
+
+getExactFilePath<-function(dirLocation)
+{
+  path<-hdfs.ls(dirLocation)
+  if(length(path$file)==1)
+  {
+    return(path$file)
+  }
+  else
+  {
+    print(paste0("More than one file exists in the location - ",path$file,".  Please specify the exact file location and set it in inputHdfsFile paramater"))
+  }
+  
+  
+}
+
+
--- R/Initialize.R
+++ R/Initialize.R
@@ -1,23 +1,19 @@
 
-# Copyright 2016 Infosys Limited
-# Licensed to the Software Freedom Conservancy (SFC) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The SFC licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# (c) 2015 Infosys Limited. All rights reserved.
+#  Except for any free or open source software  components embedded in this Infosys
+#  Proprietary software program("Program"), this Program is  protected by copyright
+#  laws, International treaties and other pending or existing intellectual property
+#  rights in India,  the United  States  and other  countries.  Except as expressly
+#  permitted, any unauthorized  reproduction  storage, photocopying,  recording  or
+#  otherwise),  or any distribution  of  this  Program,  or any portion of  it, may
+#  result  in  severe  civil  and criminal penalties, and will be prosecuted to the
+#  maximum extent possible under the law.
 
-IIP.init<-function(username)
+
+# Authentication Type 
+#                     - plain - password is stored in variable
+#                     - encrpyt - Password is encrypted and is stored in file
+IIP.init<-function(username,password = NULL)
 {
   
   library(rhdfs)
@@ -25,45 +21,16 @@ IIP.init<-function(username)
   library(RCurl)
   library(RJSONIO)
   library(RJDBC)
-  Sys.setenv(SECURITY=T)
-  sysname<-Sys.info()[["sysname"]]
+  
+  
   Sys.setenv(USERNAME=username)
-   pwdFile<-paste0("~/.",username)
-   Sys.setenv(ENCRYPT_FILE=pwdFile)
-    if(file.exists(pwdFile) & is.object(tryCatch(connect<-IIP.JDBCConnection(),error = function(err) return(length(err)))))
-    {
-      # directly fetch the file 
-      Sys.setenv(ENCRYPT_FILE=pwdFile)
-      
-    }
-    else
-    {
-      # get the password from user
-      ifRstudio<- Sys.getenv("RSTUDIO")
-      if(ifRstudio == "1")
-      {
-        password<-.rs.askForPassword("")
-        
-        
-      }
-      
-      else{
-        if(sysname=="Linux")
-        {
-          password<-get_password()
-        }
-        else{
-          password<- mask()
-        }
-      }
-      
-      IIP.encrypt(pwd = password)
-      #Sys.setenv(ENCRYPT_FILE=file)
-    }
-   Sys.setenv(USERNAME=username)
   hdfsPath<-Sys.getenv("R_HDFS_HOME")
   if(hdfs.exists(hdfsPath))
   {
+    if(substr(hdfsPath,nchar(hdfsPath),nchar(hdfsPath))!="/")
+    {
+      hdfsPath<-paste0(hdfsPath,"/")
+    }
     if(hdfs.exists(paste0(hdfsPath,username)))
     {
       print(paste0("Your HDFS working directory is  ",paste0(hdfsPath,username)))
@@ -71,46 +38,80 @@ IIP.init<-function(username)
     else{
       hdfs.mkdir(path=paste0(hdfsPath,username))
       print(paste0("Your HDFS working directory is  ",paste0(hdfsPath,username)))
-  }
-  Sys.setenv("USER_WORKSPACE"=paste0(hdfsPath,username))  
+    }
+    Sys.setenv("USER_WORKSPACE"=paste0(hdfsPath,username))  
   }
   else{
-    stop(print(paste0("Please ask Hadoop admin to create the HDFS working directory"),hdfsPath))
+    stop(print(paste0("Please ask Hadoop admin to create the HDFS working directory",hdfsPath)))
   }
   localPath<-createLocalDirectory(paste0(getwd(),"/"),"data")
   
   Sys.setenv(LOCAL_HOME=localPath)
+  
+  # check for autheticatioon type
+  
+  if(!(is.null(password)))
+  {
+    Sys.setenv(SECURITY=F)
+   
+    Sys.setenv("PWD" = password)
+  }
+  
+  else
+  {
+    Sys.setenv(SECURITY=T)
+    # encryption
+    pwdFile<-paste0(Sys.getenv("USER_WORKSPACE"),"/.",username)
+    Sys.setenv(ENCRYPT_FILE=pwdFile)
+    if(hdfs.exists(pwdFile) & is.object(tryCatch(connect<-IIP.JDBCConnection(),error = function(err) return(length(err)))))
+    {
+      # directly fetch the file 
+      Sys.setenv(ENCRYPT_FILE=pwdFile)
+      
+      
+    }
+    else
+    {
+      IIP.encrypt(pwd = getPasswordFromUser())
+      #Sys.setenv(ENCRYPT_FILE=file)
+    }
+  }
+  
+  
+  
+  
 }
 
 #for windows machines
-mask <- function() {
-  library(tcltk) 
-  tt<-tktoplevel() 
-  Password <- tclVar("") 
-  entry.Password <-tkentry(tt,width="20",textvariable=Password,show="*") 
-  tkgrid(tklabel(tt,text="Please enter your password.")) 
-  tkgrid(entry.Password) 
-  OnOK <- function() 
-  { 
-    tkdestroy(tt)  
-    Password <<- tclvalue(Password) 
-    cat("The password was ", Password, "\n") 
-  } 
-  OK.but <-tkbutton(tt,text="   OK   ",command=OnOK) 
-  tkbind(entry.Password, "<Return>",OnOK) 
-  tkgrid(OK.but) 
-  tkfocus(tt) 
-}
+get_ui_pass<-function(){  
+  library(tcltk);  
+  window<-tktoplevel();
+  password<-tclVar("");  
+    
+  tkgrid(tklabel(window,text="Enter password:"));  
+   
+  tkgrid(tkentry(window,textvariable=password,show="*")->passwordPopUp);  
+   
+  tkbind(passwordPopUp,"<Return>",function() tkdestroy(window));  
+   
+  tkgrid(tkbutton(window,text="OK",command=function() tkdestroy(window)));  
+    
+  tkwait.window(window);  
+  pwd<-tclvalue(password);  
+  return(pwd);  
+}  
+
 
 #for console
-get_password <- function() {
-  cat("Password: ")
+get_console_pwd <- function() {
+  system("echo Enter Password: ")
   system("stty -echo")
-  a <- readline()
+  pwd <- readline()
   system("stty echo")
-  cat("\n")
-  return(a)
+  #print("\n")
+  return(pwd)
 }
+
 
 
 #function to encrypt passwords using key(RSA 2048bits) for encryption
@@ -128,7 +129,7 @@ IIP.encrypt <- function(pwd=NULL,pwdFile=NULL,encryptedFilePath=NULL) {
   }
   
   if(!is.null(pwdFile)) {
-    if(!file.exists(pwdFile)) {
+    if(!hdfs.exists(pwdFile)) {
       stop(paste(pwdFile,'file does not exists!',sep=" "))
     }
     else {
@@ -149,11 +150,14 @@ IIP.encrypt <- function(pwd=NULL,pwdFile=NULL,encryptedFilePath=NULL) {
   
   #internally store private key in a local file
   PKI.save.key(key, format = c("PEM","DER"), private = TRUE, paste0("~/.private.key"))
-  keyPath<-paste0("~/.private.key")
+  system(paste0("$HADOOP_CMD fs -put -f ~/.private.key ",Sys.getenv("USER_WORKSPACE")))
+  keyPath<-paste0(Sys.getenv("USER_WORKSPACE"),"/.private.key")
   #create encryted password file
   if(!is.null(encryptedFilePath)) {
     #cat(encryptedPWD,file = encryptedFilePath, sep="\n")
     writeBin(encryptedPWD, encryptedFilePath)
+    # write the file to hdfs
+    system(paste("$HADOOP_CMD fs -put -f",encryptedFilePath,Sys.getenv("USER_WORKSPACE"), sep=" "))
     path<-encryptedFilePath
     #message(paste('Encrypted password file created at', encryptedFilePath, sep = " "))
   }
@@ -161,13 +165,25 @@ IIP.encrypt <- function(pwd=NULL,pwdFile=NULL,encryptedFilePath=NULL) {
     #cat(encryptedPWD,file="encryptedPwd.txt",sep="\n")
     path<-paste0("~/.",Sys.getenv("USERNAME"))
     writeBin(encryptedPWD, path)
-    
+    system(paste("$HADOOP_CMD fs -put -f",path,Sys.getenv("USER_WORKSPACE"), sep=" "))
     #message(paste('Encrypted password file created at ',getwd(),"/encryptedPwd.txt",sep = ""))
   }
-  Sys.setenv(ENCRYPT_FILE=path)
-  slaves<-readLines(paste0(Sys.getenv("SPARK_HOME"),"/conf/slaves"))
-  lapply(slaves,function(x){system(paste0("scp ",path," ",Sys.getenv("USERNAME"),"@",x,":",path))})
-  lapply(slaves,function(x){system(paste0("scp ",keyPath," ",Sys.getenv("USERNAME"),"@",x,":",keyPath))})
+  Sys.setenv(ENCRYPT_FILE=paste0(Sys.getenv("USER_WORKSPACE"),"/.",Sys.getenv("USERNAME")))
+  system("rm ~/.private.key")
+  system(paste0("rm ~/.",Sys.getenv("USERNAME")))
+  
+  
+  tryCatch(slaves<-readLines(paste0(Sys.getenv("SPARK_HOME"),"/conf/slaves")),
+           warning = function(w) {return("Warning message")},
+           error = function(e) {return("Unable to read slaves file")})
+  tryCatch(lapply(slaves,function(x){system(paste0("scp ",path," ",Sys.getenv("USERNAME"),"@",x,":",path))}),
+           warning = function(w) {return("Warning message")},
+           error = function(e) {return("In case you are using IIPR as part of a R Map reduce job, please execute IIP.init(<username>) on each of the data nodes.")})
+  tryCatch(lapply(slaves,function(x){system(paste0("scp ",keyPath," ",Sys.getenv("USERNAME"),"@",x,":",keyPath))}),
+           warning = function(w) {return("Warning message")},
+           error = function(e) {return("In case you are using IIPR as part of a R Map reduce job, please execute IIP.init(<username>) on each of the data nodes.")})
+  
+   
   #return(path)
 }
 
@@ -179,25 +195,29 @@ IIP.decrypt <- function(encryptedPwdFile=NULL, privateKeyFile=NULL) {
   if(is.null(encryptedPwdFile)) {
     stop('encryptedPwdFile must be provided!')
   }
-  if(!file.exists(encryptedPwdFile)) {
+  if(!hdfs.exists(encryptedPwdFile)) {
     stop(paste(encryptedPwdFile,'file does not exists!',sep=" "))
   }
   if(is.null(privateKeyFile)) {
-    privateKeyFile <- paste0("~/.private.key")
+    keyPath<-paste0(Sys.getenv("USER_WORKSPACE"),"/.private.key")
+    privateKeyFile <- pipe(paste0("$HADOOP_CMD dfs -cat ",keyPath))
+    
   }
   
   #load private key
-  private.key <- PKI.load.key(format = c("PEM","DER"),private = TRUE, file=privateKeyFile)
+  private.key <- PKI.load.key(format = c("PEM","DER"),private = TRUE, what=privateKeyFile )
   
   #load encrypted pwd file
-  encryptedPwd <- readBin(encryptedPwdFile, "raw", file.info(encryptedPwdFile)$size * 1.1)
+  system(paste0("$HADOOP_CMD dfs -copyToLocal ",encryptedPwdFile, " ~/.",Sys.getenv("USERNAME")))
+  filePath<-paste0("~/.",Sys.getenv("USERNAME"))
+  encryptedPwd <- readBin(filePath, "raw", file.info(filePath)$size * 1.1)
+  system(paste0("rm ~/.",Sys.getenv("USERNAME")))
   
   #decrypt password
-  decryptedPwd <- PKI.decrypt(encryptedPwd, private.key)
+  decryptedPwd <- PKI.decrypt(encryptedPwd,private.key)
   
   return(rawToChar(decryptedPwd))
 }
-
 
 
 
