284478b4435d8c2527512022db72797fab4e60a2
Poorva Agrawal <poorva_agrawal@infosys.com>
2017-07-13 14:13:30 +0530

    Committing 2.3

--- R/Core_Utilities.R
+++ R/Core_Utilities.R
@@ -1,22 +1,12 @@
-
-# Copyright 2016 Infosys Limited
-# Licensed to the Software Freedom Conservancy (SFC) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The SFC licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
+# (c) 2015 Infosys Limited. All rights reserved.
+#  Except for any free or open source software  components embedded in this Infosys
+#  Proprietary software program("Program"), this Program is  protected by copyright
+#  laws, International treaties and other pending or existing intellectual property
+#  rights in India,  the United  States  and other  countries.  Except as expressly
+#  permitted, any unauthorized  reproduction  storage, photocopying,  recording  or
+#  otherwise),  or any distribution  of  this  Program,  or any portion of  it, may
+#  result  in  severe  civil  and criminal penalties, and will be prosecuted to the
+#  maximum extent possible under the law.
 
 
 getPasswordFromUser<-function()
--- R/Utility.R
+++ R/Utility.R
@@ -1,23 +1,15 @@
 
 
+# (c) 2015 Infosys Limited. All rights reserved.
+#  Except for any free or open source software  components embedded in this Infosys
+#  Proprietary software program("Program"), this Program is  protected by copyright
+#  laws, International treaties and other pending or existing intellectual property
+#  rights in India,  the United  States  and other  countries.  Except as expressly
+#  permitted, any unauthorized  reproduction  storage, photocopying,  recording  or
+#  otherwise),  or any distribution  of  this  Program,  or any portion of  it, may
+#  result  in  severe  civil  and criminal penalties, and will be prosecuted to the
+#  maximum extent possible under the law.
 
-# Copyright 2016 Infosys Limited
-# Licensed to the Software Freedom Conservancy (SFC) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The SFC licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 
 createUploadTableJson<-function(dataSource,workspaceName,columns,dataType,hdfsDelimiter,filepath,tableName, fileType, roleId)
 {
@@ -118,46 +110,8 @@ IIP.loadTable<-function(dataSource,workspaceName,columns,dataType,tableName,hdfs
   ## Registration of Table
   schema<-paste0(unlist(strsplit(columns,",")),":",unlist(strsplit(dataType,",")),collapse=",")
   
-  if(fileType== "parquet")
-  {
-    driverClassName<-"org.apache.spark.sql.parquet"
-  }
-  
-  else
-  {
-    driverClassName<-"com.databricks.spark.csv"
-  }
-  # 
-  
-  
-  
-  query<-paste0('REGISTERJOB USING JSON { "name":','"',tableName,'"',
-                ',"details": {
-                "tables": [
-{
-                "schema":','"',schema,'"',",",
-                '"delimiter":','"',hdfsDelimiter,'"',",\n",
-                '"location": ','"',filepath,'"',",",
-                '"fileType": ','"',fileType,'"',",",
-                '"tableName": ','"',tableName,'"',",",
-                '"driverClassName": ','"',driverClassName,'"
-}
-                ],
-                "queries": [
-{
-                "query": "",
-                "registerAsTable": "",
-                "location": "",
-                "materialized": false
-}
-                ]
-}
-}')
-
-  
   #cred<-unlist(strsplit(credentials,":"))
   connection <- IIP.JDBCConnection()
-  dbGetQuery(connection,query)
   query<-paste0("select count(*) from ",tableName)
   print(paste("Table",tableName," created with ",dbGetQuery(connection,query)," rows"))
   }
@@ -218,8 +172,9 @@ IIP.uploadTable<-function(dataSource,workspaceName,hdfsDelimiter=",",dataFrame,t
       
       tableDecription<-dbGetQuery(connection,paste0("Describe ",tableName)) 
       tableCol<-paste(tableDecription$col_name,collapse=",")
-      
+      tableCol <- tolower(tableCol)
       columns<-paste((colnames(dataFrame)),collapse = ",")
+      columns <- tolower(columns)
       columns<-gsub("\\.","_",columns)
       factor<-sapply(dataFrame,is.factor)
       index<-as.numeric(which(factor==T))
@@ -251,9 +206,10 @@ IIP.uploadTable<-function(dataSource,workspaceName,hdfsDelimiter=",",dataFrame,t
       {
         fileName=paste0("dataFrame.",fileType)
         tablePath=paste0(tableName,".",fileType)
+        hdfsPath <- paste0(Sys.getenv("USER_WORKSPACE"),"/",tableName,"/")
         localFileLocation<-paste0(Sys.getenv("LOCAL_HOME"),"/",fileName)
         write.table(dataFrame,localFileLocation,row.names=F,col.names = F,sep =hdfsDelimiter)
-        system (paste("$HADOOP_CMD fs -appendToFile",localFileLocation,paste0(Sys.getenv("USER_WORKSPACE"),"/",tablePath)))
+        system (paste("$HADOOP_CMD fs -appendToFile",localFileLocation,paste0(hdfsPath,tablePath)))
         
         query<-paste0("select count(*) from ",tableName)
         print(paste("Table",tableName," appended and have a total of ",dbGetQuery(connection,query)," rows"))
@@ -312,44 +268,16 @@ uploadTableToIIP<-function(dataSource,workspaceName,hdfsDelimiter=",",dataFrame,
   if(id == ""){
     stop(paste0("The user - ",Sys.getenv("USERNAME"), "doesn't have", role, "privledges"))}
  
-  uploadToHDFS(localFileLocation,Sys.getenv("USER_WORKSPACE"))
-  json<-createUploadTableJson(dataSource,workspaceName,columns,dataType,hdfsDelimiter,paste0(Sys.getenv("USER_WORKSPACE"),"/",fileName),tableName, fileType, id)
-  
+  hdfsPath <- createHdfsDirectory(Sys.getenv("USER_WORKSPACE"),paste0("/",tableName,"/"))
+  uploadToHDFS(localFileLocation,hdfsPath)
+  json<-createUploadTableJson(dataSource,workspaceName,columns,dataType,hdfsDelimiter,hdfsPath,tableName, fileType, id)
   result<- casConnector(json,"uploadTable")
   print(result)
   
   
-  ## Registration of Table
-  
-  schema<-paste0(unlist(strsplit(columns,",")),":",unlist(strsplit(dataType,",")),collapse=",")
-  query<-paste0('REGISTERJOB USING JSON { "name":','"',tableName,'"',
-                '"details": {
-                "tables": [
-{
-                "schema":','"',schema,'"',",",
-                '"delimiter":','"',hdfsDelimiter,'"',",\n",
-                '"location": ','"',paste0(Sys.getenv("USER_WORKSPACE"),"/",fileName),'"',",",
-                '"fileType": ','"',fileType,'"',",",
-                '"tableName": ','"',tableName,'"',",",
-                '"driverClassName": "com.databricks.spark.csv"
-}
-                ],
-                "queries": [
-{
-                "query": "",
-                "registerAsTable": "",
-                "location": "",
-                "materialized": false
-}
-                ]
-}
-}'
-  )
-  
-  
+ 
   
   connection <- IIP.JDBCConnection()
-  dbGetQuery(connection,query)
   
   res<-fromJSON(result)
   if(res=="SUCCESS"){
@@ -382,8 +310,6 @@ colnames(roles) = c("id", "role")
 
 casConnector<-function(json, functionCall)
 {
-  
- 
   
   .jinit()
   .jaddClassPath(Sys.getenv("IIPR_CAS_JAR"))
